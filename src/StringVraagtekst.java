fac85b31b8c6e4db2e0250f838255e887296414f
sony3256 <sony3256@live.be>
2018-07-13 11:29:00 +0200

    aanpassing1

--- src/StringVraagtekst.java
+++ src/StringVraagtekst.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class StringVraagtekst {
     public static void main(String[] args) {
         Scanner invoer = new Scanner(System.in);
-        System.out.println("Geef een zin in hoofdletters");
+        System.out.println("Geef een VOLLEDIGE zin in hoofdletters");
         String tekst = invoer.nextLine();
         tekst = tekst.toUpperCase();
         System.out.println(tekst);
